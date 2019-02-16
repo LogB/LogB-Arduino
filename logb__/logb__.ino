a161d9e0ff97c0fa3d1ba39548e8517f88817b92
cimbi21 <34192798+cimbi21@users.noreply.github.com>
2019-02-16 23:20:13 +0100

    Create logb__.ino

---
+++ logb__/logb__.ino
@@ -0,0 +1,30 @@
+#include "logb.h"
+#include "Sodaq_SHT2x.h"
+
+Settings set;
+void setup() {
+Serial.begin(115200); 
+Wire.begin();
+//WiFi.begin("LogB Cloud", "logb2018");
+WiFi.begin("Potyka", "62Botticelli2016");
+delay(5000);
+set.device_id="cimbi21_1";
+set.pin="admin";
+set.where="ac";
+set.timeInterval=3000;
+CreateName(UnixTime(1));
+AddNewHeaderParam("SHT21-I2C-3V3-TEMP-C", "Temperature");
+AddNewHeaderParam("SHT21-I2C-3V3-HUM-%", "Humidity");
+Send();
+
+}
+void loop() {
+set.currentMillis = millis(); 
+if (set.currentMillis - set.previousMillis >= set.timeInterval) { 
+set.previousMillis = set.currentMillis; 
+Time(UnixTime(1));
+AddNewSensorData("SHT21-I2C-3V3-TEMP-C", String(SHT2x.GetTemperature()));
+AddNewSensorData("SHT21-I2C-3V3-HUM-%", String(SHT2x.GetHumidity()));
+Send();
+}
+}
