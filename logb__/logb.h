24df3d2c5a3595350f56a9e60433fffec73ee8da
cimbi21 <34192798+cimbi21@users.noreply.github.com>
2019-02-16 23:24:50 +0100

    Update logb.h

--- logb__/logb.h
+++ logb__/logb.h
@@ -6,9 +6,8 @@
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
-#include <time.h>
 #endif
-
+#include <time.h>
 #include <SPI.h>
 #include <Arduino.h>
 #include <Wire.h>
@@ -20,28 +19,39 @@ struct Settings{
   bool toComma=true; 
   String where="";
   String pin="";
-  String date="";
-  int db=0;
-  int now=1;
-  String device="";
+  String measurement_arduinoname="";
+  int sensor_count=0;
+  String device_id="";
   String store[25];
+  String sensors[25];
+  String header[25];
   unsigned long previousMillis = 0;
   unsigned long timeInterval= 1000;
   unsigned long currentMillis=0;
+  //time_t time;
+  int now=0;
+  bool DB=false;
+  String date;
+  String ArduinoName;
 };
+void Time(time_t time);
+void AddNewSensorData(String id, String data);
+void CreateName(time_t time);
 extern Settings set;
 void dateTime(uint16_t* date, uint16_t* time);
 void out(String data);
 String Date();
+void Send();
 String GPSread();
 String fileNameCreate();
 void timeSetting();
-String Time();
+//String Time();
 String DIPconfig();
 long int interval(String b);
 void databaseCreate();
 String onlineDate(int tz);
 int upload();
-String TableName(int tz);
+time_t UnixTime(int tz);
 void connectToDB();
 void getData(String data);
+void AddNewHeaderParam(String id, String header);
