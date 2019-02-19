0fe6f0271f98aa7ba614c5bbfcfbd123c3bb64ab
cimbi21 <34192798+cimbi21@users.noreply.github.com>
2019-02-19 20:17:40 +0100

    Update logb__.ino

--- logb__/logb__.ino
+++ logb__/logb__.ino
@@ -1,22 +1,25 @@
 #include "logb.h"
 #include "Sodaq_SHT2x.h"
-
+#include "BH1750.h"
 Settings set;
+BH1750 lightMeter;
+
 void setup() {
 Serial.begin(115200); 
 Wire.begin();
+lightMeter.begin();
 //WiFi.begin("LogB Cloud", "logb2018");
 WiFi.begin("Potyka", "62Botticelli2016");
 delay(5000);
 set.device_id="cimbi21_1";
 set.pin="admin";
 set.where="ac";
-set.timeInterval=40000;
+set.timeInterval=4000;
 CreateName(UnixTime(1));
 AddNewHeaderParam("SHT21-I2C-3V3-TEMP-C", "Temperature");
 AddNewHeaderParam("SHT21-I2C-3V3-HUM-%", "Humidity");
+AddNewHeaderParam("BH1750-I2C-3V3-LIGHT-lx", "Light");
 Send();
-
 }
 void loop() {
 set.currentMillis = millis(); 
@@ -25,6 +28,7 @@ set.previousMillis = set.currentMillis;
 Time(UnixTime(1));
 AddNewSensorData("SHT21-I2C-3V3-TEMP-C", String(SHT2x.GetTemperature()));
 AddNewSensorData("SHT21-I2C-3V3-HUM-%", String(SHT2x.GetHumidity()));
+AddNewSensorData("BH1750-I2C-3V3-LIGHT-lx", String(lightMeter.readLightLevel()));
 Send();
 }
 }
