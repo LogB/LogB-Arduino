f69d490b44c617d1dff8abf11e6d9a0443fae454
cimbi21 <34192798+cimbi21@users.noreply.github.com>
2019-03-09 19:11:31 +0100

    Update logb.cpp

--- logb__/logb.cpp
+++ logb__/logb.cpp
@@ -51,68 +51,70 @@ set.date= date;
 }
 
 void Send(){
-  String fulldata;
-  if(set.DB==false){
-#if defined(ESP8266)//ha cloudra kimaradt
-http.begin("http://cloud.logb.hu/cloud/upload.php");
-http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-String post="oszlop="+String(set.sensor_count)+"&ma="+set.ArduinoName+"&pin="+set.pin+"&device="+set.device_id+"&time="+set.date;
-        for(int i=0;i<set.sensor_count;i++){
-        post+="&logb"+String(i+1)+"="+set.header[i];
-        post+="&sensor"+String(i+1)+"="+set.sensors[i];
-        }
-int httpCode=http.POST(post);
-http.end();
-//Serial.println(httpCode);
-#endif
+String fulldata;
 
+if(set.DB==false){
 for(int i=0;i<set.sensor_count;i++){
   set.store[i]=set.header[i];
 }
-set.DB=true;
 fulldata="Date";
-  }else{
+}
+else{
     fulldata=set.date;
   }
     for (int j = 0; j < set.where.length(); j++){
-    char w = set.where.charAt(j);
-    fulldata+=set.semicolon;
-     for(int i=0;i<set.sensor_count;i++){
-      String d=set.store[i];
-        if(set.toComma){
-          d.replace('.',',');
-        }
-        fulldata+=d;
-        fulldata+=set.semicolon;
+      char w = set.where.charAt(j);
+      fulldata+=set.semicolon;
+      for(int i=0;i<set.sensor_count;i++){
+        String d=set.store[i];
+          if(set.toComma){
+            d.replace('.',',');
+           }   
+          fulldata+=d;
+          fulldata+=set.semicolon;
       }
-    if (w=='a'){
+      
+   if (w=='a'){
    Serial.println(fulldata);
     }
 
-    if (w=='b'){
+  if (w=='b'){
       SdFat SD;
-      //SdFile::dateTimeCallback(dateTime);
       File Sd = SD.open(set.file, FILE_WRITE);
         if(Sd){
           Sd.println(fulldata);
         }
         Sd.close();
-    }if(w=='c' and set.DB){
-        #if defined(ESP8266)
+    }
+    
+  if(w=='c'){
+       #if defined(ESP8266)
+        String post;
         http.begin("http://cloud.logb.hu/cloud/upload.php");
         http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-         String post="oszlop="+String(set.sensor_count)+"&ma="+set.ArduinoName+"&pin="+set.pin+"&device="+set.device_id+"&time="+set.date;
-        for(int i=0;i<set.sensor_count;i++){
-        post+="&logb"+String(i+1)+"="+set.store[i];
-      }
+        if(set.DB){
+          post="oszlop="+String(set.sensor_count)+"&ma="+set.ArduinoName+"&pin="+set.pin+"&device="+set.device_id+"&time="+set.date;
+          for(int i=0;i<set.sensor_count;i++){
+            post+="&logb"+String(i+1)+"="+set.store[i];
+          }
+        }
+          else{
+          post="oszlop="+String(set.sensor_count)+"&ma="+set.ArduinoName+"&pin="+set.pin+"&device="+set.device_id+"&time="+set.date;
+          for(int i=0;i<set.sensor_count;i++){
+            post+="&logb"+String(i+1)+"="+set.header[i];
+            post+="&sensor"+String(i+1)+"="+set.sensors[i];
+          }
+        }
         int httpCode=http.POST(post);
         http.end();
         //Serial.println(httpCode);
-         #endif
+       #endif
     }
  
     }
+    set.DB=true;
 }
+
 void AddNewSensorData(String id, String data){
   int sensorID=-1;
   for (int i=0;i<set.sensor_count ;i++){
