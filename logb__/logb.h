3a38616d882472bb50b1db8cd1b65f665b0c4dae
cimbi21 <34192798+cimbi21@users.noreply.github.com>
2019-03-15 20:46:44 +0100

    Update logb.h

--- logb__/logb.h
+++ logb__/logb.h
@@ -3,10 +3,15 @@
 #if defined(ESP8266)
 
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
-#include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
 #endif
+
+#include "Adafruit_SI1145.h"
+#include "Adafruit_BMP085.h"
+#include "RTClib.h"
+#include "BH1750.h"
+#include "Sodaq_SHT2x.h"
+
 #include <time.h>
 #include <SPI.h>
 #include <Arduino.h>
@@ -16,28 +21,26 @@
 struct Settings{
   String file=""; 
   String seperate=";";
-  bool toComma=true; 
+  bool toComma=false; 
   String where="";
   String pin="";
   String measurement_arduinoname="";
   int sensor_count=0;
-  String device_id="";
+  String device_id="logb";
   String store[25];
   String sensors[25];
   String header[25];
   unsigned long previousMillis = 0;
   unsigned long timeInterval= 1000;
   unsigned long currentMillis=0;
-  //time_t time;
-  int now=0;
   bool DB=false;
   String date;
   String ArduinoName;
 };
 extern Settings set;
-void Time(time_t time);
+void Time(DateTime time);
 void AddNewSensorData(String id, String data);
-void CreateName(time_t time);
+void CreateName(DateTime time);
 void Send();
-time_t UnixTime(int tz);
+DateTime UnixTime(int tz);
 void AddNewHeaderParam(String id, String header);
