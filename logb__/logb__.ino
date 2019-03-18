e8cb3282eb2d09ce51aed6ff7d600545601fe499
cimbi21 <34192798+cimbi21@users.noreply.github.com>
2019-03-18 15:54:04 +0100

    Update logb__.ino

--- logb__/logb__.ino
+++ logb__/logb__.ino
@@ -1,16 +1,11 @@
 #include "logb.h"
- unsigned long previousMillis = 0;
-  //unsigned long timeInterval= 1000;
-  unsigned long currentMillis=0;
-//#include "DS3231.h"
+#include "BH1750.h"
+#include "Sodaq_SHT2x.h"
 Settings set;
 BH1750 lightMeter;
 RTC_DS3231 rtc;
 void setup() {
-  Serial.begin(115200);
-      
-   
-    
+Serial.begin(115200);
 Wire.begin();
 //lightMeter.begin();
 rtc.begin();
@@ -20,25 +15,26 @@ rtc.begin();
 set.device_id="cimbi21_1";
 set.pin="admin";
 set.where="ab";
-//set.timeInterval=2000;
-
 CreateName(rtc.now());
 AddNewHeaderParam(1, "Temperature");
 AddNewHeaderParam(2, "Humidity");
-
 Send();
 }
-
 void loop() {
-currentMillis = millis(); 
-if (currentMillis - previousMillis >= 2000) { 
-previousMillis = currentMillis; 
+set.currentMillis = millis(); 
+if (set.currentMillis - set.previousMillis >= 2000) { 
+set.previousMillis = set.currentMillis; 
 set.date=rtc.now();
-AddNewSensorData(1, String(SHT2x.GetTemperature()));
-AddNewSensorData(2, String(SHT2x.GetHumidity()));
-Serial.println(rtc.now().unixtime());
-//AddNewSensorData("BH1750-I2C-3V3-LIGHT-lx", String(lightMeter.readLightLevel()));
-
+AddNewSensorData("Temperature",   String(SHT2x.GetTemperature()));
+AddNewSensorData("2Temperature",  String(SHT2x.GetHumidity()));
+AddNewSensorData("2Temperature",  String(SHT2x.GetHumidity()));
+AddNewSensorData("2Tsemperature", String(SHT2x.GetHumidity()));
+AddNewSensorData("2Tesmperature", String(SHT2x.GetHumidity()));
+AddNewSensorData("2Temsperature", String(SHT2x.GetHumidity()));
+AddNewSensorData("2Tempearature", String(SHT2x.GetHumidity()));
+AddNewSensorData("2Temperaature", String(SHT2x.GetHumidity()));
+AddNewSensorData("2Tempercature", String(SHT2x.GetHumidity()));
+AddNewSensorData("2Temperaíture", String(SHT2x.GetHumidity()));
 Send();
 }
 }
