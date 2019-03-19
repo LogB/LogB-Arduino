2b80a0b51ac3b973e2fd13842ef4b034ca263ca3
cimbi21 <34192798+cimbi21@users.noreply.github.com>
2019-03-19 08:31:45 +0100

    Update logb.h

--- logb__/logb.h
+++ logb__/logb.h
@@ -11,24 +11,24 @@
 
 struct Settings{
   String seperate=";";
-  bool toComma=false; 
+  bool toComma=true; 
   String where="";
   String pin="";
   String measurement_arduinoname="";
   int sensor_count=0;
   String device_id="logb";
-  String store[10];
-  String sensors[10];
+  //String store[10];
   bool DB=false;
   DateTime date;
   String ArduinoName;
   unsigned long previousMillis = 0;
   unsigned long currentMillis=0;
+  String fulldata="";
+  String cloud="";
 };
 extern Settings set;
 String Time(DateTime time);
-void AddNewSensorData(String id, String data);
+void AddData(String id, String data);
 void CreateName(DateTime time);
 void Send();
 DateTime UnixTime(int tz);
-void AddNewHeaderParam(int id, String header);
