86f40f04cdbe67d82b8f356172ed738e98756bfe
cimbi21 <34192798+cimbi21@users.noreply.github.com>
2019-04-22 22:30:14 +0200

    Update logb__.ino

--- logb__/logb__.ino
+++ logb__/logb__.ino
@@ -3,33 +3,34 @@
 #include "Sodaq_SHT2x.h"
 #include "Adafruit_BMP085.h"
 Settings set;
-BH1750 lightMeter;
+//BH1750 lightMeter;
 //RTC_DS1307 rtc;
 Adafruit_BMP085 bmp;
 void setup() {
 Serial.begin(115200);
 Wire.begin();
-lightMeter.begin();
+//lightMeter.begin();
 bmp.begin();
+GSMBegin();
 //rtc.begin();
 //if(!rtc.isrunning()){rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));}
-WiFi.begin("LogB", "logb2018");
-while (WiFi.status() != WL_CONNECTED) {delay(100); Serial.print(".");}
+//WiFi.begin("LogB", "logb2018");
+//while (WiFi.status() != WL_CONNECTED) {delay(100); Serial.print(".");}
 set.device_id="cimbi21_1";
 set.pin="admin";
-set.where="abc";
+set.where="ad";
 set.timeInterval=2000;
-CreateName(UnixTime(1));
+CreateName(UnixWithGSM(2));
 }
 void loop() {
 set.currentMillis = millis(); 
 if (set.currentMillis - set.previousMillis >= set.timeInterval) { 
 set.previousMillis = set.currentMillis; 
-AddData("NTP-RTC","Date",   Time(UnixTime(1)));
+AddData("NTP-RTC","Date",   Time(UnixWithGSM(2)));
 AddData("SHT21-TEMP","Hőmérséklet",   String(SHT2x.GetTemperature()));
 AddData("SHT21-HUM","Páratartalom",  String(SHT2x.GetHumidity()));
 AddData("BMP180-PRES","Nyomás",  String(bmp.readPressure()));
-AddData("BH1750-LIGHT","Fény",  String(lightMeter.readLightLevel()));
+//AddData("BH1750-LIGHT","Fény",  String(lightMeter.readLightLevel()));
 Send();
 }
 }
